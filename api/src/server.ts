cb45631138d836e2c37541badceae76b128e2ebf
Boris Marković <bmarkovic17@outlook.com>
2021-03-26 23:13:00 +0100

    Add express framework

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,4 +1,9 @@
-console.log('it works');
+import express from 'express';
+
+const server = express();
+const port = 3000;
+
+server.listen(port, () => console.info(`Server started on http://localhost:${port}`));
 
 process.on('uncaughtException', error => {
     console.error('There was an uncaught error', error);
